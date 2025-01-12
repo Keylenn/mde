4db68aa43df00580011a229354e1d9459889672c
Keylenn <darklight_on@163.com>
2025-01-12 13:27:40 +0800

    ci: fix

--- src/components/JsonReviewer/index.tsx
+++ src/components/JsonReviewer/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-// import ReactJson from "react-json-view";
+import { useState, useEffect } from "react";
 import useDomDataTheme from "../../helper/hooks/useDomDataTheme";
 import { readJsonFile } from "../../helper/util";
 import showToast from "../../helper/toast";
@@ -8,6 +7,20 @@ import "./index.css";
 const JsonReviewer = () => {
   const [theme] = useDomDataTheme();
   const [jsonData, setJsonData] = useState({});
+
+  const [ReactJson, setReactJson] = useState(null);
+
+  useEffect(() => {
+    // 动态导入 react-json-view 组件
+    import("react-json-view")
+      .then((module) => {
+        // 使用 setJsonViewer 来更新状态，这样会触发组件重新渲染
+        setReactJson(() => module.default);
+      })
+      .catch((error) => {
+        console.error("Failed to dynamically import react-json-view", error);
+      });
+  }, []); // 空依赖数组表示这个 effect 只会在组件挂载时运行一次
 
   return (
     <>
@@ -26,19 +39,21 @@ const JsonReviewer = () => {
         accept=".json"
       />
 
-      {/* <ReactJson
-        src={jsonData}
-        theme={theme === "light" ? "rjv-default" : "monokai"}
-        onEdit={(edit) => {
-          console.log("编辑内容:", edit);
-        }}
-        onAdd={(add) => {
-          console.log("新增内容:", add);
-        }}
-        onDelete={(del) => {
-          console.log("删除内容:", del);
-        }}
-      /> */}
+      {ReactJson && (
+        <ReactJson
+          src={jsonData}
+          theme={theme === "light" ? "rjv-default" : "monokai"}
+          onEdit={(edit) => {
+            console.log("编辑内容:", edit);
+          }}
+          onAdd={(add) => {
+            console.log("新增内容:", add);
+          }}
+          onDelete={(del) => {
+            console.log("删除内容:", del);
+          }}
+        />
+      )}
     </>
   );
 };
